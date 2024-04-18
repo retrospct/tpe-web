4bda473349224f8456fd2b9166bb2f914e3c09ea
Justin Lee <me@jlee.cool>
2024-04-17 21:19:46 -0700

    wip: services component

--- src/slices/NavItem/index.tsx
+++ src/slices/NavItem/index.tsx
@@ -1,5 +1,6 @@
-import { Content } from '@prismicio/client'
-import { SliceComponentProps } from '@prismicio/react'
+import { Content, isFilled } from '@prismicio/client'
+import { PrismicNextLink } from '@prismicio/next'
+import { PrismicText, SliceComponentProps } from '@prismicio/react'
 
 /**
  * Props for `NavItem`.
@@ -12,7 +13,11 @@ export type NavItemProps = SliceComponentProps<Content.NavItemSlice>
 const NavItem = ({ slice }: NavItemProps): JSX.Element => {
   return (
     <li data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
-      Placeholder component for nav_item (variation: {slice.variation}) Slices
+      {isFilled.link(slice.primary.link) && (
+        <PrismicNextLink field={slice.primary.link}>
+          {isFilled.richText(slice.primary.name) && <PrismicText field={slice.primary.name} />}
+        </PrismicNextLink>
+      )}
     </li>
   )
 }
