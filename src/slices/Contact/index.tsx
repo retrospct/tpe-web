a6d2c05baadfb897b9dc92f2268bc627cd7a2391
Justin Lee <me@jlee.cool>
2024-04-19 21:11:28 -0700

    feat: adds contact form slice component

--- src/slices/Contact/index.tsx
+++ src/slices/Contact/index.tsx
@@ -16,10 +16,10 @@ const Contact = ({ slice }: ContactProps): JSX.Element => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className="my-16 flex w-full flex-col items-center justify-between text-red sm:my-24 md:flex-row"
+      className="mx-auto my-16 max-w-7xl text-red sm:my-24"
     >
       {slice.variation === 'default' && (
-        <>
+        <div className="flex flex-col items-center justify-center text-red lg:flex-row">
           <div className="flex w-full flex-col items-center justify-between">
             {isFilled.richText(slice.primary.title) && (
               <div className="mx-3 flex items-center justify-center text-center font-serif text-4xl font-normal tracking-wider text-red lg:mx-6 lg:text-5xl">
@@ -30,14 +30,14 @@ const Contact = ({ slice }: ContactProps): JSX.Element => {
             )}
             <p className="mt-11">Form goes here</p>
           </div>
-          <div className="max-w-1/2 lg:max-w-1/3 relative mx-3 mt-12 w-full text-center lg:mx-6 lg:mt-0">
+          <div className="relative mx-3 mt-12 w-full max-w-[360px] text-center lg:mx-6 lg:mt-0 lg:max-w-[500px]">
             <PrismicNextImage
               field={slice.primary.image}
               imgixParams={{ crop: 'faces,edges', fit: 'crop', w: 1, h: 1 }}
               className="object-cover"
             />
           </div>
-        </>
+        </div>
       )}
     </section>
   )
