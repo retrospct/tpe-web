7ac965ea1d4a986c3b122937176fab05f9edc20d
Justin Lee <me@jlee.cool>
2024-05-03 17:07:34 -0700

    chore: payload 3.0 beta installed

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -7,14 +7,14 @@ import { PrismicPreview } from '@prismicio/next'
 import { Crimson_Pro } from 'next/font/google'
 import localFont from 'next/font/local'
 
-import '../styles/globals.css'
+import '../../styles/globals.css'
 
 const crimson = Crimson_Pro({
   subsets: ['latin'],
   variable: '--font-crimson'
 })
 
-const belgant = localFont({ src: './font/belgant-aesthetic.otf', variable: '--font-belgant' })
+const belgant = localFont({ src: '../font/belgant-aesthetic.otf', variable: '--font-belgant' })
 
 export default function RootLayout({
   children
