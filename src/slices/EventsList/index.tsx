109f7c3007cad7fbefc91cce175d67d5dc24a1cc
Justin Lee <me@jlee.cool>
2024-06-05 20:13:26 -0700

    fix: portfolio anchor links

--- src/slices/EventsList/index.tsx
+++ src/slices/EventsList/index.tsx
@@ -11,16 +11,20 @@ export type EventsListProps = SliceComponentProps<Content.EventsListSlice>
  * Component for "EventsList" Slices.
  */
 const EventsList = ({ slice }: EventsListProps): JSX.Element => {
-  const anchorLink = isFilled.keyText(slice.primary.anchor_link)
-    ? slice.primary.anchor_link.toLowerCase().replace(/\s/g, '-')
-    : slice.slice_type
-
   return (
     <section data-slice-type={slice.slice_type} data-slice-variation={slice.variation} className="my-10 w-full">
       {slice.variation === 'default' && (
-        <div id={anchorLink} className="flex w-full flex-col items-center justify-center">
+        <div className="flex w-full flex-col items-center justify-center">
           {isFilled.richText(slice.primary.title) && (
-            <HeadingDivider richText={slice.primary.title} anchor={anchorLink} className="max-w-4xl" />
+            <HeadingDivider
+              richText={slice.primary.title}
+              anchor={
+                isFilled.keyText(slice.primary.anchor_link)
+                  ? slice.primary.anchor_link.toLowerCase().replace(/\s/g, '-')
+                  : slice.slice_type
+              }
+              className="max-w-4xl"
+            />
           )}
           <EventsGrid events={slice?.items} />
         </div>
