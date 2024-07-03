428d3fe862f3a5e72bdb960daa5208c9328b7994
Justin Lee <me@jlee.cool>
2024-07-03 16:46:31 -0700

    fix: schema updates

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -6,17 +6,17 @@ import * as schema from './schema'
 export const db = drizzle(sql, { schema })
 
 export const getUsers = async () => {
-  const selectResult = await db.select().from(schema.UsersTable)
+  const selectResult = await db.select().from(schema.users)
   console.log('Results', selectResult)
-  return db.query.UsersTable.findMany()
+  return db.query.users.findMany()
 }
 
 export const getContactForm = async () => {
-  const selectResult = await db.select().from(schema.ContactFormTable)
+  const selectResult = await db.select().from(schema.contactForm)
   console.log('Results', selectResult)
-  return db.query.ContactFormTable.findMany()
+  return db.query.contactForm.findMany()
 }
 
 export const createContactForm = async (data: schema.InsertContactForm) => {
-  return await db.insert(schema.ContactFormTable).values(data)
+  return await db.insert(schema.contactForm).values(data).onConflictDoNothing()
 }
--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,10 +1,12 @@
 // import { drizzle } from 'drizzle-orm/vercel-postgres';
 // import { sql } from '@vercel/postgres';
-import { boolean, date, index, jsonb, pgTable, text, timestamp, uniqueIndex, uuid, varchar } from 'drizzle-orm/pg-core'
+import { boolean, date, jsonb, pgTable, text, timestamp, uniqueIndex, uuid, varchar } from 'drizzle-orm/pg-core'
 
-export const UsersTable = pgTable(
+// tpe users & admins
+export const users = pgTable(
   'users',
   {
+    // id: serial('id').primaryKey(),
     id: uuid('id').defaultRandom().primaryKey(),
     email: varchar('email', { length: 256 }).notNull().unique(),
     name: varchar('name', { length: 256 }).notNull(),
@@ -17,28 +19,87 @@ export const UsersTable = pgTable(
     }
   }
 )
+export type InsertUsers = typeof users.$inferInsert
+export type SelectUsers = typeof users.$inferSelect
 
-export const ContactFormTable = pgTable(
+// // customers & other persons
+// export const persons = pgTable(
+//   'persons',
+//   {
+//     id: uuid('id').defaultRandom().primaryKey(),
+//     name: varchar('name', { length: 256 }).notNull(),
+//     email: varchar('email', { length: 256 }).notNull(),
+//     phone: varchar('phone', { length: 256 }).notNull(),
+//     eventDate: date('eventDate'),
+//     referral: varchar('referral', { length: 256 }),
+//     // newsletter: boolean('newsletter').default(false),
+//     comments: text('comments'),
+//     createdAt: timestamp('createdAt').defaultNow().notNull()
+//   },
+//   (persons) => {
+//     return {
+//       personIdx: index('person_idx').on(persons.email)
+//     }
+//   }
+// )
+// export type InsertPersons = typeof persons.$inferInsert
+// export type SelectPersons = typeof persons.$inferSelect
+
+// export const personsRelations = relations(persons, ({ many }) => ({
+//   contactForm: many(contactForm),
+//   subscriptions: many(subscriptions)
+// }))
+
+export const contactForm = pgTable(
   'contact_form',
   {
-    // id: serial('id').primaryKey(),
     id: uuid('id').defaultRandom().primaryKey(),
-    name: varchar('name', { length: 256 }).notNull(),
+    // pid: uuid('pid')
+    //   .references(() => persons.id, { onDelete: 'cascade' })
+    //   .notNull(),
     email: varchar('email', { length: 256 }).notNull(),
+    name: varchar('name', { length: 256 }).notNull(),
     phone: varchar('phone', { length: 256 }).notNull(),
-    eventDate: date('eventDate'),
-    comments: text('comments'),
     referral: varchar('referral', { length: 256 }),
+    eventDate: date('eventDate'),
     newsletter: boolean('newsletter').default(false),
+    comments: text('comments'),
     raw: jsonb('raw'),
     createdAt: timestamp('createdAt').defaultNow().notNull()
-  },
-  (contact_form) => {
-    return {
-      contactIdx: index('contact_idx').on(contact_form.email)
-    }
   }
+  // (contact_form) => {
+  //   return {
+  //     contactIdx: index('contact_idx').on(contact_form.email)
+  //   }
+  // }
 )
+export type InsertContactForm = typeof contactForm.$inferInsert
+export type SelectContactForm = typeof contactForm.$inferSelect
 
-export type InsertContactForm = typeof ContactFormTable.$inferInsert
-export type SelectContactForm = typeof ContactFormTable.$inferSelect
+// export const contactFormRelations = relations(contactForm, ({ one }) => ({
+//   author: one(persons, {
+//     fields: [contactForm.pid],
+//     references: [persons.id]
+//   })
+// }))
+
+// export const subscriptions = pgTable(
+//   'contact_form',
+//   {
+//     id: uuid('id').defaultRandom().primaryKey(),
+//     pid: uuid('pid')
+//       .references(() => persons.id, { onDelete: 'cascade' })
+//       .notNull(),
+//     newsletter: boolean('newsletter').default(false),
+//     createdAt: timestamp('createdAt').defaultNow().notNull()
+//   }
+// )
+// export type InsertSubscriptions = typeof subscriptions.$inferInsert
+// export type SelectSubscriptions = typeof subscriptions.$inferSelect
+
+// export const subscriptionsRelations = relations(subscriptions, ({ one }) => ({
+//   subscriber: one(persons, {
+//     fields: [subscriptions.pid],
+//     references: [persons.id]
+//   })
+// }))
