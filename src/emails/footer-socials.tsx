2bcbf3da2eaed9a5e829608fde66ca82b52c7e2f
Justin Lee <me@jlee.cool>
2024-07-05 18:18:10 -0700

    fix: social footer in email showing up in dark mode

--- src/emails/footer-socials.tsx
+++ src/emails/footer-socials.tsx
@@ -6,29 +6,59 @@ export const FooterSocials = () => {
   return (
     <Section>
       <Row>
-        <Column width={38}>
+        <Column width={40}>
           <Link href="https://www.yelp.com/biz/two-perfect-events-palo-alto">
-            <Img src={`${baseUrl}/img/socials/logo-yelp@2x.png`} width="28" height="28" alt="Yelp logo" />
+            <Img
+              src={`${baseUrl}/img/socials/logo-yelp@2x.png`}
+              width="28"
+              height="28"
+              alt="Yelp logo"
+              className="rounded-full bg-white p-[5px]"
+            />
           </Link>
         </Column>
-        <Column width={38}>
+        <Column width={40}>
           <Link href="https://www.instagram.com/twoperfectevents">
-            <Img src={`${baseUrl}/img/socials/logo-instagram@2x.png`} width="28" height="28" alt="Instagram logo" />
+            <Img
+              src={`${baseUrl}/img/socials/logo-instagram@2x.png`}
+              width="28"
+              height="28"
+              alt="Instagram logo"
+              className="rounded-full bg-white p-[5px]"
+            />
           </Link>
         </Column>
-        <Column width={38}>
+        <Column width={40}>
           <Link href="https://www.facebook.com/twoperfectevents">
-            <Img src={`${baseUrl}/img/socials/logo-facebook@2x.png`} width="28" height="28" alt="Facebook logo" />
+            <Img
+              src={`${baseUrl}/img/socials/logo-facebook@2x.png`}
+              width="28"
+              height="28"
+              alt="Facebook logo"
+              className="rounded-full bg-white p-[5px]"
+            />
           </Link>
         </Column>
-        <Column width={38}>
+        <Column width={40}>
           <Link href="https://www.tiktok.com/@twoperfectevents">
-            <Img src={`${baseUrl}/img/socials/logo-tiktok@2x.png`} width="28" height="28" alt="Tiktok logo" />
+            <Img
+              src={`${baseUrl}/img/socials/logo-tiktok@2x.png`}
+              width="28"
+              height="28"
+              alt="Tiktok logo"
+              className="rounded-full bg-white p-[5px]"
+            />
           </Link>
         </Column>
-        <Column width={38}>
+        <Column width={40}>
           <Link href="https://www.pinterest.com/twoperfectevents">
-            <Img src={`${baseUrl}/img/socials/logo-pinterest@2x.png`} width="28" height="28" alt="Pinterest logo" />
+            <Img
+              src={`${baseUrl}/img/socials/logo-pinterest@2x.png`}
+              width="28"
+              height="28"
+              alt="Pinterest logo"
+              className="rounded-full bg-white p-[5px]"
+            />
           </Link>
         </Column>
         <Column />
