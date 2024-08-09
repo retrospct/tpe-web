e637e5d063a87fcb7d2df8a27c6ae5dfc24c1a3d
Justin Lee <me@jlee.cool>
2024-08-09 14:44:58 -0700

    fix: updated images and construct metadata

--- src/lib/utils/functions/construct-metadata.ts
+++ src/lib/utils/functions/construct-metadata.ts
@@ -5,6 +5,7 @@ interface ConstructMetadataProps {
   title?: string
   description?: string
   image?: string | null
+  imageSquared?: string | null
   icons?: Metadata['icons']
   noIndex?: boolean
   previousImages?: any
@@ -13,17 +14,18 @@ interface ConstructMetadataProps {
 export function constructMetadata({
   title = `${APP_NAME} - Chic + Multicultural Weddings & Events for Creative Souls`,
   description = `${APP_NAME} is a Palo Alto-based event planning studio, crafting unique, unforgettable weddings and events. We specialize in personalized details and flawless execution, ensuring every celebration reflects your unique style and creates lasting memories.`, // 'A full-service event planning company based in Palo Alto, CA.'
+  // url, // optional canonical url
   image = '/social-card.jpg', // 1866x980 on dub, we use 1200x630
+  // imageAlt = og:image:alt
+  // imageDimensions = { width: 1200, height: 630 },
+  imageSquared = '/social-card_squared.jpg', // 1200x1200
+  // imageSquaredAlt = og:image:alt
+  // imageAltDimensions = { width: 1200, height: 1200 },
   icons = [
     {
       rel: 'icon',
       sizes: 'any',
       url: '/favicon.ico'
-    },
-    {
-      rel: 'apple-touch-icon',
-      sizes: '32x32',
-      url: '/apple-touch-icon.png'
     },
     {
       rel: 'icon',
@@ -36,6 +38,11 @@ export function constructMetadata({
       type: 'image/png',
       sizes: '16x16',
       url: '/favicon-16x16.png'
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '32x32',
+      url: '/apple-touch-icon.png'
     }
   ],
   noIndex = false,
@@ -48,7 +55,13 @@ export function constructMetadata({
     openGraph: {
       title,
       description,
-      ...(image && { images: [{ url: image }, ...previousImages] })
+      ...(image && {
+        images: [
+          { url: image, width: 1200, height: 630 },
+          { url: imageSquared, width: 1200, height: 1200 },
+          ...previousImages
+        ]
+      })
     },
     twitter: {
       title,
@@ -66,16 +79,12 @@ export function constructMetadata({
 //   title: `${APP_NAME} - Chic + Multicultural Weddings & Events for Creative Souls`,
 //   description: `${APP_NAME} is a Palo Alto-based event planning studio, crafting unique, unforgettable weddings and events. We specialize in personalized details and flawless execution, ensuring every celebration reflects your unique style and creates lasting memories.`, // 'A full-service event planning company based in Palo Alto, CA.'
 //   image: 'https://twoperfectevents.com/social-card.jpg', // 1866x980 on dub, we use 1200x630
+//   imageSquared = '/social-card_squared.jpg', // 1200x1200
 //   icons: [
 //     {
 //       rel: 'icon',
 //       sizes: 'any',
 //       url: '/favicon.ico'
-//     },
-//     {
-//       rel: 'apple-touch-icon',
-//       sizes: '32x32',
-//       url: '/apple-touch-icon.png'
 //     },
 //     {
 //       rel: 'icon',
@@ -88,6 +97,11 @@ export function constructMetadata({
 //       type: 'image/png',
 //       sizes: '16x16',
 //       url: '/favicon-16x16.png'
+//     },
+//     {
+//       rel: 'apple-touch-icon',
+//       sizes: '32x32',
+//       url: '/apple-touch-icon.png'
 //     }
 //   ],
 //   noIndex: false
