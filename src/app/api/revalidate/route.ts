20f32cbb52776199e0801d8de45795d6655c9497
Justin Lee <me@jlee.cool>
2024-08-20 17:52:50 -0700

    fix: adds revalidateTag functionality

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,8 +1,18 @@
 import { revalidateTag } from 'next/cache'
-import { NextResponse } from 'next/server'
+import { type NextRequest, NextResponse } from 'next/server'
 
-export async function POST() {
-  revalidateTag('prismic')
-
-  return NextResponse.json({ revalidated: true, now: Date.now() })
+export async function POST(req: NextRequest) {
+  try {
+    const res = await req.json()
+    console.log('res.body', res.body)
+    const { secret } = res.body
+    if (secret !== process.env.REVALIDATE_SECRET) return res.status(401).json({ message: 'Invalid token' })
+    revalidateTag('prismic')
+    return NextResponse.json({ revalidated: true, now: Date.now() })
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({ status: 400, message: `Webhook error: ${error.message}` })
+    }
+    return NextResponse.json({ status: 500, message: 'Unknown webhook error...' })
+  }
 }
