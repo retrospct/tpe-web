8f743b44ccaf97c1c1f6bbc655639114cc070dc7
Justin Lee <me@jlee.cool>
2024-08-23 13:35:28 -0700

    feat: add posthog analytics for session replay

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,10 +8,12 @@ import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Crimson_Pro } from 'next/font/google'
 import localFont from 'next/font/local'
+import { PHProvider } from './providers'
 
 import dynamic from 'next/dynamic'
 import '../styles/globals.css'
 
+const PostHogPageView = dynamic(() => import('./PostHogPageView'), { ssr: false })
 const PrismicPreview = dynamic(() => import('@prismicio/next').then((mod) => mod.PrismicPreview), { ssr: false })
 
 const crimson = Crimson_Pro({ subsets: ['latin'], variable: '--font-crimson', preload: true })
@@ -31,15 +33,18 @@ export default function RootLayout({
         belgant.variable
       )}
     >
-      <body>
-        <Nav />
-        {children}
-        <Footer />
-        <Toaster position="bottom-center" richColors />
-        {process.env.NODE_ENV === 'development' && <PrismicPreview repositoryName={repositoryName ?? 'tpe-web'} />}
-        <SpeedInsights sampleRate={0.5} />
-        <Analytics />
-      </body>
+      <PHProvider>
+        <body>
+          <Nav />
+          <PostHogPageView />
+          {children}
+          <Footer />
+          <Toaster position="bottom-center" richColors />
+          {process.env.NODE_ENV === 'development' && <PrismicPreview repositoryName={repositoryName ?? 'tpe-web'} />}
+          <SpeedInsights sampleRate={0.5} />
+          <Analytics />
+        </body>
+      </PHProvider>
       <GoogleAnalytics gaId="G-BK1E6E3S2L" />
     </html>
   )
