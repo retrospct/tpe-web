f4e9779fee8d55d56169c87ff6df83ada04621a3
Justin Lee <me@jlee.cool>
2025-03-28 16:43:38 -0700

    fix: update email templates

--- src/emails/footer-socials.tsx
+++ src/emails/footer-socials.tsx
@@ -1,35 +1,35 @@
 import { Column, Img, Link, Row, Section } from '@react-email/components'
 
-const baseUrl = process.env.NEXT_PUBLIC_APP_HOST ? `https://email.${process.env.NEXT_PUBLIC_APP_HOST}` : '/static'
+const baseUrl = process.env.NEXT_PUBLIC_APP_HOST ? `https://${process.env.NEXT_PUBLIC_APP_HOST}` : '/static'
 
 const socials = [
   {
     name: 'yelp',
-    url: `https://email.twoperfectevents.com/api/resend/link/${encodeURIComponent('https://www.yelp.com/biz/two-perfect-events-palo-alto')}`,
+    url: `https://twoperfectevents.com/api/resend/link/${encodeURIComponent('https://www.yelp.com/biz/two-perfect-events-palo-alto')}`,
     img: `${baseUrl}/img/socials/logo-yelp@2x.png`,
     alt: 'Yelp logo'
   },
   {
     name: 'instagram',
-    url: `https://email.twoperfectevents.com/api/resend/link/${encodeURIComponent('https://www.instagram.com/twoperfectevents')}`,
+    url: `https://twoperfectevents.com/api/resend/link/${encodeURIComponent('https://www.instagram.com/twoperfectevents')}`,
     img: `${baseUrl}/img/socials/logo-instagram@2x.png`,
     alt: 'Instagram logo'
   },
   {
     name: 'facebook',
-    url: `https://email.twoperfectevents.com/api/resend/link/${encodeURIComponent('https://www.facebook.com/twoperfectevents')}`,
+    url: `https://twoperfectevents.com/api/resend/link/${encodeURIComponent('https://www.facebook.com/twoperfectevents')}`,
     img: `${baseUrl}/img/socials/logo-facebook@2x.png`,
     alt: 'Facebook logo'
   },
   {
     name: 'tiktok',
-    url: `https://email.twoperfectevents.com/api/resend/link/${encodeURIComponent('https://www.tiktok.com/@twoperfectevents')}`,
+    url: `https://twoperfectevents.com/api/resend/link/${encodeURIComponent('https://www.tiktok.com/@twoperfectevents')}`,
     img: `${baseUrl}/img/socials/logo-tiktok@2x.png`,
     alt: 'Tiktok logo'
   },
   {
     name: 'pinterest',
-    url: `https://email.twoperfectevents.com/api/resend/link/${encodeURIComponent('https://www.pinterest.com/twoperfectevents')}`,
+    url: `https://twoperfectevents.com/api/resend/link/${encodeURIComponent('https://www.pinterest.com/twoperfectevents')}`,
     img: `${baseUrl}/img/socials/logo-pinterest@2x.png`,
     alt: 'Pinterest logo'
   }
