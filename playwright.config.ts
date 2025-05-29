c773deb36d34fe01a4551f4e32ddb745ab4e4c18
Justin Lee <me@jlee.cool>
2025-05-29 12:04:35 -0700

    test: fixes test configurations

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,26 +1,31 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test'
 
 export default defineConfig({
   testDir: './e2e',
-  baseURL: 'http://localhost:3000',
+  use: {
+    baseURL: 'http://localhost:3000'
+  },
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'], ignoreHTTPSErrors: true },
+      use: { ...devices['Desktop Chrome'] }
+      // use: { ...devices['Desktop Chrome'], ignoreHTTPSErrors: true }
     },
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'], ignoreHTTPSErrors: true },
+      use: { ...devices['Desktop Firefox'] }
+      // use: { ...devices['Desktop Firefox'], ignoreHTTPSErrors: true }
     },
     {
       name: 'webkit',
-      use: { ...devices['Desktop Safari'], ignoreHTTPSErrors: true },
-    },
+      use: { ...devices['Desktop Safari'] }
+      // use: { ...devices['Desktop Safari'], ignoreHTTPSErrors: true }
+    }
   ],
   webServer: {
-    command: 'pnpm run next:dev',
+    command: 'pnpm run dev',
     url: 'http://localhost:3000',
-    reuseExistingServer: !process.env.CI,
-    timeout: 180 * 1000, // Increased timeout
-  },
-});
+    reuseExistingServer: !process.env.CI
+    // timeout: 180 * 1000 // Increased timeout
+  }
+})
